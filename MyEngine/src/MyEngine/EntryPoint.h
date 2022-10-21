67230df3e7ca483893aa9e89ece939b6f8963faa
Eightina <frankalation@gmail.com>
2022-10-21 18:13:27 +0800

    Log System

--- MyEngine/src/MyEngine/EntryPoint.h
+++ MyEngine/src/MyEngine/EntryPoint.h
@@ -8,7 +8,11 @@
 extern MyEngine::Application* MyEngine::CreateApplication();
 
 int main(int argc, char** argv) {
-	printf("MyEngine\n");
+	MyEngine::Log::Init();
+	ME_CORE_WARN("Initialized Log!");
+	int a = 5;
+	ME_INFO("Hello Var = {0}", a);
+
 	auto app = MyEngine::CreateApplication();
 	app->Run();
 	delete app;
